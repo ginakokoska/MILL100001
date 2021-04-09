837d91b29dfb96ff8da11bf2575920849c136deb
ginakokoska <ginkoks@gmail.com>
2021-04-09 09:34:21 +0200

    Version 0.3 by Gina

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,5 +2,6 @@ object main {
   def main(args: Array[String]): Unit = {
     println("Hello World, and Welcome to our Game!")
     println("Hello Luca thanks for the invite")
+    rintln("Hello Luca,  Welcome Ginas-Branch of Hello World!")
   }
 }
