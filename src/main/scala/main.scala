6f2f2490f6acbb992e44cfd4e2fc283a166e78a7
ginakokoska <ginkoks@gmail.com>
2021-04-09 10:22:38 +0200

    Version 0.3.1 by Gina

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,5 +4,6 @@ object main {
     println("Hello Luca thanks for the invite")
     println("Hello Luca,  Welcome Ginas-Branch of Hello World!")
     println("Hello Luca,  Welcome Ginas-2nd-Branch of Hello World!")
+    println("please allow merge")
   }
 }
