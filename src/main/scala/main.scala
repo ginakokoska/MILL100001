940759f942e78b67133e136548a16ab88c54f8dd
ginakokoska <ginkoks@gmail.com>
2021-04-16 13:09:33 +0200

    Master Branch

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,5 @@
 object main {
   def main(args: Array[String]): Unit = {
-    println("Hello World, and Welcome to our Game!")
-    println("Hello Luca thanks for the invite")
-    println("Hello Luca,  Welcome Ginas-Branch of Hello World!")
-    println("Hello Luca,  Welcome Ginas-2nd-Branch of Hello World!")
-    println("please allow merge")
+    println("Hello World, and Welcome to our Game Mill!")
   }
 }
